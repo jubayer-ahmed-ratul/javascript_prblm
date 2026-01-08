b010100f2a703be8601ec454d2e7fcb69cf8d2b2
jubayer-ahmed-ratul <ahmedjubayer.ratul@gmail.com>
2026-01-08 11:40:11 +0600

    max

---
+++ max.js
@@ -0,0 +1,13 @@
+function Max(arr) {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+let arr = [5, 1, 9, 3];
+console.log(Max(arr));
