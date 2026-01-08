30ad12e70553bbf4750afd8f0922678bce4b1042
jubayer-ahmed-ratul <ahmedjubayer.ratul@gmail.com>
2026-01-08 11:45:15 +0600

    sum

---
+++ sum.js
@@ -0,0 +1,14 @@
+function sArray(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+
+let numbers = [1, 2, 3, 4];
+
+
+console.log(sArray(numbers)); 
